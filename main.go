1eb8de6246aa402ffc4b15d53a5b5135bb2dd6f8
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-04-27 20:50:16 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"time"
+)
 
+// TODO: 开源以后完善
 func main() {
-	fmt.Println("hello github")
+	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second))
+	fmt.Println("Hello github", conn, err)
 }
