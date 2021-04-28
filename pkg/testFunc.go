7445f2b1958ec85e3ef1f1150d378be8df29b431
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-04-28 10:24:48 +0800

    ...

---
+++ pkg/testFunc.go
@@ -0,0 +1,11 @@
+package pkg
+
+import "fmt"
+
+func HelloGit(){
+	fmt.Println("hello, git")
+}
+
+func HelloWorld(){
+	fmt.Println("hello, world")
+}
---
+++ pkg/testFunc_test.go
@@ -0,0 +1,11 @@
+package pkg
+
+import "testing"
+
+func TestHelloGit(t *testing.T){
+	HelloGit()
+}
+
+func TestHelloWorld(t *testing.T) {
+	HelloWorld()
+}
