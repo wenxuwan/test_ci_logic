839ec1d23a7ccd9683dd238ba7d99fc590c97b1e
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-04-28 10:30:21 +0800

    fix

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 
 // TODO: 开源以后完善
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second))
+	conn, err := grpc.Dial("127.0.0.1:34904", grpc.WithInsecure(), grpc.WithBlock())
 	fmt.Println("Hello github", conn, err)
 }
